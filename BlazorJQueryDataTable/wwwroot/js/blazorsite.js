7ca6faf477fcb8fd2136e26848e00b8828aaafbb
Mofaggol Hoshen <mofaggol.hoshen@quipu.de>
2025-04-17 15:50:52 +0200

    blazor sample

---
+++ BlazorJQueryDataTable/wwwroot/js/blazorsite.js
@@ -0,0 +1,8 @@
+﻿function jQueryDataTable(id, data) {
+    const table = $("#" + id).DataTable();
+    table.clear();
+    if (data !== null) {
+        table.rows.add(data);
+    }
+    table.draw();
+}
